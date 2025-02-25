0235d124acbe6a5026ec3cf71ea6be164590312c
VO Cindy -DARLINGTON <volequynhanh67@gmail.com>
2025-02-25 07:45:04 -0500

    create insert method

--- LinkedlList.py
+++ LinkedlList.py
@@ -91,6 +91,21 @@ class LinkedList:
             temp.value = value
             return True
         return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            self.prepend(value)
+        if index > 0:
+            self.append(value)
+        else:
+            new_node = Node(value)
+            temp = self.get(index-1)
+            new_node.next = temp.next
+            temp.next = new_node
+            self.length += 1
+            return True
 
 
 
